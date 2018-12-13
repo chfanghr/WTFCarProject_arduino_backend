ef1388059109117a0d97652b5be7aab5e36b4f33
方泓睿 <chfanghr@gmail.com>
2018-12-13 14:25:31 +0800

    Update

--- User_Setup.h
+++ User_Setup.h
@@ -1,13 +1,2 @@
 #pragma once
 
-#define DEBUG 1
-
-#ifdef DEBUG
-#define DEGUG_INIT Serial.begin(115200)
-#define PRINT_FUNCTION Serial.println(__FUNCTION__)
-#define PRINT_DEBUG_MSG(m) Serial.print("DEBUG:");Serial.println(m)
-#else
-#define PRINT_FUNCTION 1
-#define PRINT_DEBUG_MSG 1
-#endif
-
