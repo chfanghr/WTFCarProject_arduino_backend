589f6d9d92bada113cb40ac1ae0172251b1d7a88
方泓睿 <chfanghr@gmail.com>
2018-12-13 14:23:40 +0800

    Update

--- backend_arduino.ino
+++ backend_arduino.ino
@@ -1,44 +1,31 @@
-#include <Arduino.h>
 #include <Wire.h>
-#include "User_Setup.h"
+#include <Arduino.h>
 
 const uint8_t motor_pin[] = {3/*A1*/, 5/*A2*/, 6/*B1*/, 9/*B2*/};
-const uint8_t led_pin = 13;
-const uint8_t i2c_addr = 0x30;
-
-void rec(int) {
-    PRINT_FUNCTION;
-    noInterrupts();
-    if (Wire.available() >= 4)
-        for (auto i = 0; i < 4; i++) {
-            auto v = Wire.read();
-            if (v < 0)v = 0;
-            else if (v > 255)v = 255;
-            PRINT_DEBUG_MSG(v);
-            analogWrite(motor_pin[i], v);
-        }
-    interrupts();
-}
+uint8_t motor_pwm[] = {0, 0, 0, 0};
+bool is_pwm_changed = false;
+const uint8_t i2c_addr = 9;
 
 void setup() {
-    DEGUG_INIT;
-    PRINT_FUNCTION;
-    for (auto i = 0; i < 4; i++) {
-        pinMode(motor_pin[i], OUTPUT);
-        analogWrite(motor_pin[i], 0);
-    }
-
-    pinMode(led_pin, OUTPUT);
-    digitalWrite(led_pin, HIGH);
-
-    Wire.onReceive(rec);
+    for (auto i = 0; i < 4; i++)pinMode(motor_pin[i], OUTPUT);
     Wire.begin(i2c_addr);
+    Wire.onRequest(requestEvent);
+    Wire.onReceive(receiveEvent);
+}
+
+void receiveEvent(int) {
+    while (Wire.available() < 4);
+    for (auto i = 0; i < 4; i++)motor_pwm[i] = static_cast<uint8_t>(Wire.read());
+    is_pwm_changed = true;
+}
+
+void requestEvent() {
+    Wire.write(motor_pwm, 4);
 }
 
 void loop() {
-    PRINT_FUNCTION;
-    digitalWrite(led_pin, HIGH);
-    delay(500);
-    digitalWrite(led_pin, LOW);
-    delay(500);
+    if (is_pwm_changed) {
+        for (auto i = 0; i < 4; i++)analogWrite(motor_pin[i], motor_pwm[i]);
+        is_pwm_changed = false;
+    }
 }
